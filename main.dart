87c21cf3ea6545ad3ee65a72bfe4fd23954da995
Huzaifakhan2002 <125730346+Huzaifakhan2002@users.noreply.github.com>
2023-03-10 21:07:10 +0500

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,106 @@
+// // import 'dart:_internal';
+// import 'dart:io';
+// // import 'dart:math';
+
+// void main(){
+// question#1:-
+// ANSWER:-
+
+// Map<String, double> expenses = {
+//   'sun': 3000.0,
+//   'mon': 3000.0,
+//   'tue': 3234.0,
+//   };
+// expenses.addAll({'fri':5000.0});
+
+// print(expenses);
+// }
+
+// QUESTION#2:-
+// ANSWER:-
+
+
+// void area_of_circle(pi,r){
+// double area=pi*r*r;
+// print('the area of a circle is $area');
+
+// }
+
+// void main (){
+// double pi=3.14;
+// // let the radius of circle is 12
+// double r=12;
+// area_of_circle(pi, r);
+// }
+
+// QUESTION#3:-
+// ANSWER:-
+
+// import'dart:math';
+
+// void implement(a, b, c) {
+//   double pythogorean_theorum = (c = sqrt((a * a) + (b * b)));
+//   print('Implements of Pythagorean theorem is $pythogorean_theorum');
+// }
+
+// void main() {
+//   // let the values of a,b,c is given:
+//   double a = 6;
+//   double b = 8;
+//   double c = sqrt(a * a + b * b);
+//   implement(a, b, c);
+// }
+
+// // Question#4:-
+// // answer:-
+
+// void reverse(sentence) {
+//   String names = sentence;
+//   print(names.split('').reversed.join());
+// }
+// void main() {
+//   String sentence = "my name is Muhammad Huzaifa khan";
+//   reverse(sentence);
+//  }
+
+// Question#6:-
+// answer:-
+
+// void power(certain_number){
+// int num=certain_number;
+// for (var i = 0; i <=certain_number ; i++) {
+// print(i*certain_number);  
+// }
+// }
+// void main(){
+//   int certain_number=5;
+//   power(certain_number);
+// }
+
+// //Question#7:-
+// // ANSWER:-
+// void vowel(sentence){
+//   String alphabet=sentence;
+//   int vowelCount=0;
+//   int consonantCount=0;
+//   for (var i = 0; i < alphabet.length; i++) {
+//     if(
+//       alphabet[i]=='a'||
+//       alphabet[i]=='e'||
+//       alphabet[i]=='i'||
+//       alphabet[i]=='o'||
+//       alphabet[i]=='u') {
+//         vowelCount=vowelCount+1;}
+//   else{
+//     consonantCount++;
+//   }
+//   }
+
+// print("the numbers of vowels in sentence is $vowelCount");
+// print("the numbers of consonant in sentense is $consonantCount");
+// }
+// void main(){
+//   String sentence="My name is Muhammad Huzaifa Khan";
+//   vowel(sentence);
+// }
+
